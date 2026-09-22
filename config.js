4c3d5b4bdd7d1783f1be5ca9a1ee9e787ab19e82
Aman-Singhal-1692 <amansinghal1692@gmail.com>
2026-09-22 13:10:43 +0530

    Update GOOGLE_CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 //
 // Example: const GOOGLE_CLIENT_ID = "1234567890-abc123xyz.apps.googleusercontent.com";
 
-const GOOGLE_CLIENT_ID = "YOUR_CLIENT_ID_HERE.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "656028163152-o3bnj7vou9bvt28bvmn5se0qtrli78l9.apps.googleusercontent.com";
